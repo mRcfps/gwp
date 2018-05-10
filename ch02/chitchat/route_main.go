70a4ba5d5171ddbacc25fc396c8bcea462e9e0a6
mRcfps <1402491442@qq.com>
2018-05-10 21:00:28 +0800

    Add chitchat app
    TODO: Stylesheets and js files can't be executed due to incorrect MIME type.

---
+++ ch02/chitchat/route_main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/mRcfps/gwp/ch02/chitchat/data"
+)
+
+// GET /err?msg=
+// shows the error message page
+func err(w http.ResponseWriter, r *http.Request) {
+	vals := r.URL.Query()
+	_, err := session(w, r)
+	if err != nil {
+		generateHTML(w, vals.Get("msg"), "layout", "public.navbar", "error")
+	} else {
+		generateHTML(w, vals.Get("msg"), "layout", "private.navbar", "error")
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	threads, err := data.Threads()
+	if err != nil {
+		errorMessage(w, r, "Cannot get threads")
+	} else {
+		_, err := session(w, r)
+		if err != nil {
+			generateHTML(w, threads, "layout", "public.navbar", "index")
+		} else {
+			generateHTML(w, threads, "layout", "private.navbar", "index")
+		}
+	}
+}
