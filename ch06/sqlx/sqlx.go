c5602926b01c6cd07216bc879916eaebd7169164
mRcfps <1402491442@qq.com>
2018-05-19 18:20:23 +0800

    Add example of sqlx

---
+++ ch06/sqlx/sqlx.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type Post struct {
+	ID         int
+	Content    string
+	AuthorName string `db: author`
+}
+
+var Db *sqlx.DB
+
+func init() {
+	var err error
+	Db, err = sqlx.Open("postgres", "user=mRc dbname=gwp password=gwp sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetPost(id int) (post Post, err error) {
+	post = Post{}
+	err = Db.QueryRowx("select id, content, author from posts where id = $1", id).StructScan(&post)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (post *Post) Create() (err error) {
+	err = Db.QueryRow("insert into posts (content, author) values ($1, $2) returning id", post.Content, post.AuthorName).Scan(&post.ID)
+	return
+}
+
+func main() {
+	post := Post{Content: "Hello World!", AuthorName: "mRcfps"}
+	post.Create()
+	fmt.Println(post)
+}
